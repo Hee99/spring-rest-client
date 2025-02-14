c4d207e3739f9ca61b2dcf7da03011bf8c28bf3d
Hee99 <119tpgml@gmail.com>
2025-02-14 16:11:52 +0900

    공통 요청함수 에러처리

--- web-client-practice/src/main/java/com/webclient/config/TodoApiClient.java
+++ web-client-practice/src/main/java/com/webclient/config/TodoApiClient.java
@@ -1,8 +1,12 @@
 package com.webclient.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.server.ResponseStatusException;
+import reactor.core.publisher.Mono;
 
 @Component
 @RequiredArgsConstructor
@@ -10,10 +14,22 @@ public class TodoApiClient {
 
     private final WebClient webClient;
 
+    private Mono<? extends Throwable> handleError(ClientResponse response) {
+        return response.bodyToMono(String.class)
+                .flatMap(errorBody -> Mono.error(
+                    new ResponseStatusException(response.statusCode(), errorBody)
+                ));
+    }
+
+    private boolean isError(HttpStatusCode status) {
+        return status.is4xxClientError() || status.is5xxServerError();
+    }
+
     public <T> T sendGet(String uri, Class<T> responseType) {
         return webClient.get()
                 .uri(uri)
                 .retrieve()
+                .onStatus(this::isError, this::handleError)
                 .bodyToMono(responseType)
                 .block();
     }
@@ -23,6 +39,7 @@ public class TodoApiClient {
                 .uri(uri)
                 .bodyValue(body)
                 .retrieve()
+                .onStatus(this::isError, this::handleError)
                 .bodyToMono(responseType)
                 .block();
     }
@@ -32,6 +49,7 @@ public class TodoApiClient {
                 .uri(uri)
                 .bodyValue(body)
                 .retrieve()
+                .onStatus(this::isError, this::handleError)
                 .bodyToMono(responseType)
                 .block();
     }
@@ -40,6 +58,7 @@ public class TodoApiClient {
         return webClient.delete()
                 .uri(uri)
                 .retrieve()
+                .onStatus(this::isError, this::handleError)
                 .bodyToMono(responseType)
                 .block();
     }
